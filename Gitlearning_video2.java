6eed571aa8702f99ed9f46b9cf3ee7d376d26661
Menaga Perumal <smenagaprabu@gmail.com>
2024-11-18 17:13:28 -0500

    this is seleniumsessions push

---
+++ Gitlearning_video2.java
@@ -0,0 +1,38 @@
+package gitlearning;
+
+public class Gitlearning_video2 {
+	
+	public static void main (String args[]) 
+	{
+		int i=10;
+		int j=20;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
